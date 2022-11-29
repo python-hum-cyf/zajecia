ff8d31e339f3eadf17b7df715b1fb55fb6095389
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2022-11-29 16:05:26 +0100

    Create task005.py
    Added file with task description.

---
+++ task005.py
@@ -0,0 +1,9 @@
+# Napisać funkcję przyjmującą jako parametr listę i zwracającą True, jeśli którykolwiek jej element jest listą, False w każdym innym razie.
+
+# Przykład 1:
+# Lista: [15, 'abcd', 42]
+# Zwrócony wynik: False
+
+# Przykład 2:
+# Lista: [77, [1, 2, 3], {'haslo': 'Jozef Tkaczuk'}]
+# Zwrócony wynik: True
