b6e3e2c6fbfbf11a2be3c3a98970d0dcc917288f
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2022-11-29 15:33:25 +0100

    Create task002.py
    Added file with task description.

---
+++ task002.py
@@ -0,0 +1,4 @@
+# Napisać funkcję usuwającą zduplikowane wartości z listy - można, a nawet należy zwrócić nową, najlepiej posortowaną listę!
+# Przykład:
+# Oryginalna lista: [10, 73, 10, 67, 40, 73, 50, 50, 67]
+# Lista zwracana: [10, 40, 50, 67, 73]
