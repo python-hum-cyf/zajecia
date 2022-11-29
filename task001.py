995369074544aa98b030589d282048c69515c273
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2022-11-29 15:27:38 +0100

    Create task001.py
    Added a file describing this task.

---
+++ task001.py
@@ -0,0 +1,6 @@
+# Napisać funkcję przyjmującą jako parametr listę stringów i zwracającą liczbę zawartych w niej stringów dłuższych od 2
+# i których pierwszy i ostatni znak są takie same.
+
+# Przykład:
+# lista: ['abc', 'pqr', 'ege', '5665', 'cc']
+# zwrócony wynik: 2 (kwalifikują się stringi 'ege' i '5665')
