7569b49bfd16ef247da358fcb2bd57bf2887ef9e
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2022-11-29 15:51:51 +0100

    Create task004.py
    Added file with task description

---
+++ task004.py
@@ -0,0 +1,4 @@
+# Napisać funkcję przyjmującą jako parametr listę stringów i zwracającą string będący ich połączeniem:
+# Przykład:
+# Lista: ['P', 'y', 't', 'h', 'o', 'n']
+# Zwracany wynik: 'Python'
