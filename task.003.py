8c34c23c4443df4399e11552366798ae70053c3d
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2022-11-29 15:45:07 +0100

    Create task.003.py
    Added file with task description

---
+++ task.003.py
@@ -0,0 +1,9 @@
+# Napisać funkcję przyjmującą jako parametry dwie listy i zwracającą True jeśli te listy mają choć jeden wspólny element, a False
+# w każdym innym wypadku.
+
+# Przykład 1:
+# Listy: [0, 45, 89, 'wlazl_kotek'] ['na_plotek', 89, 'i_mruga']
+# Zwrócony wynik: True
+# Przykład 2:
+# Listy: ['zaspiewaj', 966] ['piosenka_niedluga', 1410]
+# Zwrócony wynik: False
